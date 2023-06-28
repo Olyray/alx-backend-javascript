4f7679978297169c3fe59bd0dbdf44c1b0461af5
Olamide <olire96@gmail.com>
2023-06-28 20:28:49 +0100

    Substituted eachPromise.reason.message with eachPromise.reason

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -14,7 +14,7 @@ export default async function handleProfileSignup(firstName, lastName, filename)
   return waitPromises.map((eachPromise) => (
     {
       status: eachPromise.status,
-      value: eachPromise.status === 'fulfilled' ? eachPromise.value : eachPromise.reason.message,
+      value: eachPromise.status === 'fulfilled' ? eachPromise.value : eachPromise.reason,
     }
   ));
 }
