045834a34090fd1ca0940771afe61271282e4841
Olamide <olire96@gmail.com>
2023-06-28 20:19:38 +0100

    Checking the sixth task

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+// Import the necessary functions
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+// Initialise the async function
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  // Create an array of the promises using the imported functions
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ];
+  // Use promise.allsettled to wait for the promises
+  const waitPromises = await Promise.allSettled(promises);
+  // return a map of the results
+  return waitPromises.map((eachPromise) => (
+    {
+      status: eachPromise.status,
+      value: eachPromise.status === 'fulfilled' ? eachPromise.value : eachPromise.reason.message,
+    }
+  ));
+}
